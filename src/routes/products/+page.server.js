521f5a008b38537b3aa2ae221befe5bc8ede77c2
MetehanBatal <47251500+MetehanBatal@users.noreply.github.com>
2024-12-09 14:25:49 +0300

    CMS changes for demo

--- src/routes/products/+page.server.js
+++
@@ -1,5 +0,0 @@
-export async function load({ fetch, params }) {
-	const productsReq = await fetch("https://preconvert.novus.studio/prod/products/viewProducts");
-	const productRes = await productsReq.json();
-	return { productRes }
-}
--- src/routes/products/[slug]/+page.server.js
+++
@@ -1,9 +0,0 @@
-export async function load({fetch, url}) {
-	let id = url.searchParams.get("id")
-	if(id == "add-new"){
-		return {}
-	}
-	const productReq = await fetch(`https://preconvert.novus.studio/prod/products/viewProducts?id=${id}`);
-	const productRes = await productReq.json();
-	return {productRes};
-}
