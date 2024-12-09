fee1d55034adeb11cf4a2c818ad2184c400e97ca
MetehanBatal <47251500+MetehanBatal@users.noreply.github.com>
2024-12-09 14:32:57 +0300

    CMS changes for demo

--- src/routes/dashboard/+page.server.js
+++
@@ -1,6 +0,0 @@
-export async function load({ fetch, params }) {
-	const experimentsReq = await fetch("https://preconvert.novus.studio/prod/mExperiments/view?limit=100&offset=0&status=in-progress");
-	const experimentsRes = await experimentsReq.json();
-
-	return { experimentsRes };	
-}
